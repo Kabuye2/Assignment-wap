741e5dabe01443132745ce2ddb37e74542f58019
Kabuye2 <149701875+Kabuye2@users.noreply.github.com>
2024-02-07 15:05:08 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function validate (email, password){
+    if (email =="kabuye@gmail.com" && password =="22/u/60182024"){
+       return console.log ("user email is " + email);
+    }
+    else{
+        console.log ("incorrect user credentials")
+    }
+}
+
+validate("kabuye@gmail.com" ,'22/u/60186018');
